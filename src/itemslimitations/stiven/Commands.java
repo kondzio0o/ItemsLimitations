1a4a951ec5de3968378b9c2c358b2fdc5ba94178
StivenFocs <64734642+StivenFocs@users.noreply.github.com>
2024-08-26 19:04:26 +0200

    removed unused imports on the Commands class

--- src/itemslimitations/stiven/Commands.java
+++ src/itemslimitations/stiven/Commands.java
@@ -1,14 +1,10 @@
 package itemslimitations.stiven;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.util.Vector;
 
 public class Commands implements CommandExecutor {
 
